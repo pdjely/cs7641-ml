21fdb8c61b3aca4b7cc0bbfcdecfbbea18c22c1a
Paul Ely <pdely.git@gmail.com>
2019-08-30 20:55:59 +0800

    adult dataset skeleton

--- src/datajanitor/adult.py
+++ src/datajanitor/adult.py
@@ -1,6 +1,26 @@
 from . import datajanitor
 
 class Adult(datajanitor.DataJanitor):
+    """
+    Adult dataset from UCI Archive.
+
+    Classify adults into income <50k or >=50k
+
+    https://archive.ics.uci.edu/ml/datasets/Adult
+    """
     def __init__(self):
-        url = 'http://www.google.com'
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
         super().__init__(name='adult', dataUrl=url)
+
+    def formatData(self, keepCorr=False, doOHE=False, **kwargs):
+        """
+        Format adult dataset
+
+        :param keepCorr:
+        :param doOHE:
+        :param kwargs:
+        :return:
+        """
+        super().formatData()
+
+
