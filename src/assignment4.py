26b53376b4d805e5df284934477cf7e4bf52a6d2
Paul Ely <pdely.git@gmail.com>
2019-11-17 18:23:11 +0800

    PI and VI

--- src/assignment4.py
+++ src/assignment4.py
@@ -1,22 +1,41 @@
-import gym
-from gym import wrappers
-from mdptoolbox import mdp
 import numpy as np
-from A4 import policy_iteration, value_iteration
+import A4.frozenlake as frozenlake
+from mdptoolbox import mdp
+import A4.tictactoe as ttt
 
 
 print('==== Running FrozenLake ====')
-gamma = 1.0
-fl = gym.make('FrozenLake8x8-v0')
-print('Starting value iteration')
-policy, V, n_iter = value_iteration(fl, theta=1e-8, discount_factor=gamma)
-print('Converged in {} iterations'.format(n_iter))
-print(V.shape)
+gamma = 0.99
+# fl = frozenlake.custom_frozen_lake(size=10)
+# print('Starting value iteration')
+# policy, V, n_iter, r = frozenlake.value_iteration(fl, theta=1e-8, discount_factor=gamma)
+# print('Converged in {} iterations'.format(n_iter))
+# print(r)
+#
+# print('\nStarting policy iteration')
+# pi_policy, pi_V, pi_n_iter, pi_r = frozenlake.policy_iteration(fl, discount_factor=gamma)
+# print('PI converged in {} iterations'.format(pi_n_iter))
+# print(pi_r)
+#
+# print('PI and VI converged to same policy? {}'
+#       .format(np.all(policy == pi_policy)))
 
-print('\nStarting policy iteration')
-pi_policy, pi_V, pi_n_iter = policy_iteration(fl, discount_factor=gamma)
-print('PI converged in {} iterations'.format(pi_n_iter))
-print(pi_V)
+print('====== Running Tic Tac Toe =======')
 
-print('PI and VI converged to same policy? {}'
-      .format(np.all(policy == pi_policy)))
+print('\nValue Iteration')
+P, R = ttt.getTransitionAndRewardArrays()
+ttt_vi = mdp.ValueIteration(P, R, 0.99)
+ttt_vi.setVerbose()
+ttt_vi.run()
+
+print(f'MDP Toolbox VI finished in {ttt_vi.iter} iterations')
+print(f'Accumulated reward: {len(ttt_vi.rewards)}')
+print(f'Rewards: {ttt_vi.rewards}')
+
+print('\nPolicy Iteration')
+ttt_pi = mdp.PolicyIteration(P, R, 0.99)
+ttt_pi.setVerbose()
+ttt_pi.run()
+print(f'MDP Toolbox PI finished in {ttt_pi.iter} iterations')
+print(f'Accumulated reward: {len(ttt_pi.rewards)}')
+print(f'Rewards: {ttt_pi.rewards}')
